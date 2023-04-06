d4a6ed38ae0974c392e83ac729dc76a7883685d8
jelemux <jeremias.weber@protonmail.com>
2023-04-07 00:35:40 +0200

    Use flake for devShell only

--- flake.nix
+++ flake.nix
@@ -25,47 +25,10 @@
         ];
       };
 
-      # Our supported systems are the same supported systems as the Zig binaries
-      systems = builtins.attrNames inputs.zig.packages;
-
-      pname = "zig-representer";
-      version = "0.0.0";
-
       zigVersion = "0.10.1";
       z = pkgs.zigpkgs.${zigVersion};
 
-      zig_representer = pkgs.stdenv.mkDerivation {
-        inherit pname version;
-        src = ./.;
-        nativeBuildInputs = with pkgs; [ z ];
-        buildInputs = with pkgs; [ ];
-        dontConfigure = true;
-
-        preBuild = ''
-          export HOME=$TMPDIR
-        '';
-
-        installPhase = ''
-          runHook preInstall
-          zig build -Drelease-safe --prefix $out install
-          runHook postInstall
-        '';
-
-        installFlags = ["DESTDIR=$(out)"];
-
-        meta = with pkgs.lib; {
-          description = "An Exercism representer for the Zig programming language";
-          license = licenses.agpl3Only;
-          platforms = platforms.linux;
-          maintainers = with maintainers; [ jelemux ];
-        };
-      };
-
     in rec {
-      packages = {
-        default = zig_representer;
-      };
-
       devShells.default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
           docker
